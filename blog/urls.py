7b3dcca11d2df2305f4e5bf8f8a664d470528ab1
Muhammed Ali <muhamzyali@gmail.com>
2020-05-29 11:16:14 +0100

    url

--- blog/urls.py
+++ blog/urls.py
@@ -1,17 +1,17 @@
-from django.conf.urls import url, path
+from django.urls import include, path, re_path
 from . import views
 
 
 urlpatterns = [
     path('',views.PostListView.as_view(),name='post_list'),
-    url(r'^about/$',views.AboutView.as_view(),name='about'),
-    url(r'^post/(?P<pk>\d+)$', views.PostDetailView.as_view(), name='post_detail'),
-    url(r'^post/new/$', views.CreatePostView.as_view(), name='post_new'),
-    url(r'^post/(?P<pk>\d+)/edit/$', views.PostUpdateView.as_view(), name='post_edit'),
-    url(r'^drafts/$', views.DraftListView.as_view(), name='post_draft_list'),
-    url(r'^post/(?P<pk>\d+)/remove/$', views.PostDeleteView.as_view(), name='post_remove'),
-    url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
-    url(r'^post/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
-    url(r'^comment/(?P<pk>\d+)/approve/$', views.comment_approve, name='comment_approve'),
-    url(r'^comment/(?P<pk>\d+)/remove/$', views.comment_remove, name='comment_remove'),
+    re_path(r'^about/$',views.AboutView.as_view(),name='about'),
+    re_path(r'^post/(?P<pk>\d+)$', views.PostDetailView.as_view(), name='post_detail'),
+    re_path(r'^post/new/$', views.CreatePostView.as_view(), name='post_new'),
+    re_path(r'^post/(?P<pk>\d+)/edit/$', views.PostUpdateView.as_view(), name='post_edit'),
+    re_path(r'^drafts/$', views.DraftListView.as_view(), name='post_draft_list'),
+    re_path(r'^post/(?P<pk>\d+)/remove/$', views.PostDeleteView.as_view(), name='post_remove'),
+    re_path(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
+    re_path(r'^post/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
+    re_path(r'^comment/(?P<pk>\d+)/approve/$', views.comment_approve, name='comment_approve'),
+    re_path(r'^comment/(?P<pk>\d+)/remove/$', views.comment_remove, name='comment_remove'),
 ]
